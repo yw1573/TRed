4bb680fe2adb8b235238d5023b2bf6e9ed8d2379
yw1573 <yw1573@yeah.net>
2024-03-21 10:24:36 +0800

    添加返回键提示退出逻辑

--- app/src/main/java/com/yw1573/tred/MainActivity.kt
+++ app/src/main/java/com/yw1573/tred/MainActivity.kt
@@ -2,6 +2,8 @@ package com.yw1573.tred
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,23 +12,35 @@ import androidx.navigation.ui.setupWithNavController
 
 
 class MainActivity : AppCompatActivity() {
+    private var backPressedTime: Long = 0
+    private lateinit var backToast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_enter,
-                R.id.navigation_table,
-                R.id.navigation_chart,
-                R.id.navigation_setup
+                R.id.navigation_enter, R.id.navigation_table, R.id.navigation_chart, R.id.navigation_setup
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
+        backToast = Toast.makeText(this, "再按一次返回退出应用", Toast.LENGTH_SHORT)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (backPressedTime + 2000 > System.currentTimeMillis()) {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                } else {
+                    backToast.show()
+                    backPressedTime = System.currentTimeMillis()
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 }
